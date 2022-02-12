2cf51c753978c8c6a79d8156d8a63fdcdbdfb103
Victor Miclotte <victor.miclotte@gmail.com>
2022-02-12 14:44:55 +0100

    [4-median-two-sorted-arrays/examples] Fix: simplify and improve examples code

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import mediantwosortedarrays "leetcode/4-median-two-sorted-arrays"
+import mediantwosortedarrays "leetcode/0004-median-two-sorted-arrays"
 
 func main() {
 	mediantwosortedarrays.Examples()
