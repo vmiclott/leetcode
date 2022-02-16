9872e4d8c0de4bae1e4803b4191d4ffe2c4d38dc
Victor Miclotte <victor.miclotte@gmail.com>
2022-02-16 18:46:40 +0100

    [9-palindrome-number] Feature: improved time complexity from O(log10 n) to O(1)

--- 9-palindrome-number/solution.go
+++ 9-palindrome-number/solution.go
@@ -7,20 +7,15 @@ func isPalindrome(x int) bool {
 	if x < 10 {
 		return true
 	}
+	if x%10 == 0 {
+		return false
+	}
 
-	digits := []int{}
-	for x > 0 {
-		digits = append(digits, x%10)
+	reverted := 0
+	for x > reverted {
+		reverted = 10*reverted + x%10
 		x /= 10
 	}
-	i, j := 0, len(digits)-1
-	for i < j {
-		if digits[i] != digits[j] {
-			return false
-		}
-		i++
-		j--
-	}
 
-	return true
+	return x == reverted || x == reverted/10
 }
