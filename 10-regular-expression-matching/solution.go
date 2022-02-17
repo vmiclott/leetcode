4d3bff4f2e206400614a03cab3dae449b857a216
Victor Miclotte <victor.miclotte@gmail.com>
2022-02-17 22:50:39 +0100

    [10-regular-expression-matching] Feature: use dynamic programming

--- 10-regular-expression-matching/solution.go
+++ 10-regular-expression-matching/solution.go
@@ -1,15 +1,30 @@
 package regularexpressionmatching
 
 func isMatch(s string, p string) bool {
-	if len(p) == 0 {
-		return len(s) == 0
+	memo := map[int]map[int]bool{}
+
+	var d func(i int, j int) bool
+	d = func(i int, j int) bool {
+		if _, ok := memo[i]; !ok {
+			memo[i] = map[int]bool{}
+		}
+		if _, ok := memo[i][j]; ok {
+			return memo[i][j]
+		}
+		if j == len(p) {
+			memo[i][j] = i == len(s)
+			return memo[i][j]
+		}
+
+		match := i < len(s) && (p[j] == s[i] || p[j] == '.')
+		if j+1 < len(p) && p[j+1] == '*' {
+			memo[i][j] = d(i, j+2) || (match && d(i+1, j))
+			return memo[i][j]
+		}
+
+		memo[i][j] = match && d(i+1, j+1)
+		return memo[i][j]
 	}
 
-	match := len(s) > 0 && (p[0] == s[0] || p[0] == '.')
-
-	if len(p) >= 2 && p[1] == '*' {
-		return isMatch(s, p[2:]) || (match && isMatch(s[1:], p))
-	}
-
-	return match && isMatch(s[1:], p[1:])
+	return d(0, 0)
 }
