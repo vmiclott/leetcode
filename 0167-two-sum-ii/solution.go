c48c196b1a190d59128ced531357c96a2fe451e8
Victor Miclotte <victor.miclotte@gmail.com>
2022-06-10 13:32:40 +0200

    [0167-two-sum-ii] Feature: added solution

---
+++ 0167-two-sum-ii/solution.go
@@ -0,0 +1,21 @@
+package twosumii
+
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+
+	var sum int
+
+	for i < j {
+		sum = numbers[i] + numbers[j]
+		if sum == target {
+			return []int{i + 1, j + 1}
+		}
+		if sum < target {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	return []int{-1, -1}
+}
