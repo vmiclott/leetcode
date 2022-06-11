6197a376f70239617579d44f8c48b3055d2c6c10
Victor Miclotte <victor.miclotte@gmail.com>
2022-06-11 12:07:49 +0200

    [1658-min-ops-to-reduce-to-zero] Feature: added examples

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
+	minopstoreducetozero "leetcode/1658-min-ops-to-reduce-to-zero"
 )
 
 func main() {
-	fmt.Println("Check out the branch \"solution/<number>\" to run the examples for problem <number>.")
+	minopstoreducetozero.Examples()
 }
