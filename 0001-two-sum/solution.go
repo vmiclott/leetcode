73314b69ee150e1ba8cb7a07aa1bc9e5e299264e
Victor Miclotte <victor.miclotte@gmail.com>
2022-07-10 10:28:15 +0200

    [0001-two-sum] Change: solution with hashmap

--- 0001-two-sum/solution.go
+++ 0001-two-sum/solution.go
@@ -1,12 +1,13 @@
 package twosum
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
 		}
+		m[v] = i
 	}
 	return nil
 }
